c6b0e981bf35043df5f78033c7040df513faba6f
Aperjump <wei@aperjump.me>
2018-08-13 14:33:50 -0700

    restructure project

---
+++ Demo1/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/weiwang/Work/cmake_test/Demo1
+# Build directory: /Users/weiwang/Work/cmake_test/Demo1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(TutorialRuns "Tutorial" "25")
+add_test(TutorialComps25 "Tutorial" "25")
+subdirs("MathFunctions")
